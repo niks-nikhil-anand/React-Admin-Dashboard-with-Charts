95ba30ed1fe2949177f2e370a24250bb9c4b5998
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-23 03:34:59 +0530

    done with the dashboard search bar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,20 @@
 import AdminSideBar from "../component/AdminSideBar"
+import { FaSearch , FaBell} from "react-icons/fa";
+import userImg from '../assets/person.png'
 
 const dashboard = () => {
   return (
     <div className="adminContainer">
       <AdminSideBar/>
-      <main>
-        Nkhil this is the main tag
+      <main className="dashboard">
+        
+       <div className="bar">
+       <FaSearch />
+       <input type="text" placeholder="Search for data, users, docs "/>
+       <FaBell />
+       <img src= {userImg} alt="img" />
+        </div>
+
       </main>
     </div>
   )
