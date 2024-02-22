5d0763e89fece62df9b29dcbd40629a853a432e9
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-23 02:51:01 +0530

    Admin SideBar is completed

--- src/component/AdminSideBar.tsx
+++ src/component/AdminSideBar.tsx
@@ -1,11 +1,13 @@
 import { IconType } from "react-icons";
 import {Link ,Location , useLocation} from 'react-router-dom'
-import { MdDashboard , MdProductionQuantityLimits } from "react-icons/md";
-import { FaShoppingBag } from "react-icons/fa";
+import { MdDashboard } from "react-icons/md";
+import { FaShoppingBag , FaChartBar , FaChartPie } from "react-icons/fa";
 import { IoIosPerson } from "react-icons/io";
 import { GrTransaction } from "react-icons/gr";
-
-
+import { LuLineChart } from "react-icons/lu";
+import { IoStopwatch } from "react-icons/io5";
+import { RiCoupon5Fill } from "react-icons/ri";
+import { LuCoins } from "react-icons/lu";
 
 
 const AdminSideBar = () => {
@@ -13,7 +15,18 @@ const AdminSideBar = () => {
   return (
     <aside>
       <h2>Logo</h2>
-      <div>
+      <Dashboard location={location} />
+      <Charts location={location} />
+      <Apps location={location} />
+    </aside>
+  )
+};
+
+
+// Dashboard component
+
+const Dashboard = ({location} : {location : Location}) => (
+  <div>
         <h5>Dashboard</h5>
         <ul>
           <Li
@@ -42,9 +55,65 @@ const AdminSideBar = () => {
           />
         </ul>
       </div>
-    </aside>
-  )
-};
+) 
+
+// Charts Components 
+
+const Charts = ({location} : {location : Location}) =>(
+      <div>
+        <h5>Charts</h5>
+        <ul>
+          <Li
+          url="/admin/chart/bar" 
+          text="Bar"
+          Icon={FaChartBar}
+          location={location}
+          />
+          <Li
+          url="/admin/chart/pie" 
+          text="Pie"
+          Icon={FaChartPie}
+          location={location}
+          />
+          <Li
+          url="/admin/chart/line" 
+          text="Line"
+          Icon={LuLineChart}
+          location={location}
+          />
+         
+        </ul>
+      </div>
+)
+
+// Addons Apps
+
+const Apps = ({location} : {location : Location})=> (
+  <div>
+        <h5>Apps</h5>
+        <ul>
+          <Li
+          url="/admin/app/stopwatch" 
+          text="Stopwatch"
+          Icon={IoStopwatch}
+          location={location}
+          />
+          <Li
+          url="/admin/app/coupon" 
+          text="Coupon"
+          Icon={RiCoupon5Fill}
+          location={location}
+          />
+          <Li
+          url="/admin/app/toss" 
+          text="Toss"
+          Icon={LuCoins}
+          location={location}
+          />
+          
+        </ul>
+      </div>
+)
 
 interface LiProps {
   url : string ,
